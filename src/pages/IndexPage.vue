79a6e98a209fe05dc38b4d4b559e12c6a17442e1
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-03-08 15:55:23 +0000

    feat: add functions store

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,10 @@
+<script setup>
+import { useFunctionsStore } from 'stores/functions.js'
+</script>
+
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px" />
+    <q-btn @click="useFunctionsStore().test()">Test</q-btn>
   </q-page>
 </template>
-
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
-</script>
