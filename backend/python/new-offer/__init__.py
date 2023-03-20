aa9b1438d2202856ece7b03e7241e30e91d9d367
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-03-20 19:54:03 +0000

    refactor(new-offer): function code

--- backend/python/new-offer/__init__.py
+++ backend/python/new-offer/__init__.py
@@ -1,5 +1,8 @@
 import logging
+import azure.functions as func
 
-def main(context: dict, new_offers: list) -> None:
-    logging.info('Python HTTP trigger function processed a request.')
-    context['bindings']['signalRMessages'] = [{'target': 'newOffer', 'arguments': [offer]} for offer in new_offers]
+def main(new_offers: func.DocumentList):
+    if new_offers:
+        logging.info('First document Id modified: %s', new_offers[0]['id'])
+
+    # context['bindings']['signalRMessages'] = [{'target': 'newOffer', 'arguments': [offer]} for offer in new_offers]
