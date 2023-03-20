ec399e5bc7b5a6741be2fa4421a29ad5348b38ed
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-03-20 19:01:33 +0000

    feat(new-offers): function logic

--- backend/python/new-offer/__init__.py
+++ backend/python/new-offer/__init__.py
@@ -1,24 +1,5 @@
 import logging
 
-import azure.functions as func
-
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
+def main(context: dict, new_offers: list) -> None:
     logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    context['bindings']['signalRMessages'] = [{'target': 'newOffer', 'arguments': [offer]} for offer in new_offers]
