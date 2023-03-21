be18c3d7aa904f9fbb3b6de9f45566b945ee51e9
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-03-21 13:07:37 +0000

    refactor(read-offers): list cosmosdb entries

--- backend/python/read-offers/__init__.py
+++ backend/python/read-offers/__init__.py
@@ -1,6 +1,17 @@
 import azure.functions as func
 import logging
+import json
 
 
-def main(req: func.HttpRequest, connection) -> func.HttpResponse:
-    return func.HttpResponse(connection)
+def main(req: func.HttpRequest, doc:func.DocumentList, connection) -> func.HttpResponse:
+    offers_json = []
+    for offer in doc:
+        offers_json.append({
+            "id": offer['id'],
+        #    "name": offer['name']
+        })
+
+    return func.HttpResponse(
+            json.dumps({"conn": connection, "offers": offers_json}),
+            status_code=200,
+            mimetype="application/json")
