233be97d40d49f0155c544a71ec81c0335750c24
az00379 <az00379@surrey.ac.uk>
2023-04-03 17:15:50 +0100

    refactor: made much needed changes to the codebase

--- frontend/src/pages/FarmersDashboard.vue
+++ frontend/src/pages/FarmersDashboard.vue
@@ -1,33 +1,70 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
-        <div class="text-center mb-4">
-          <h1>Dashboard</h1>
+      <!-- Map and Weather Widgets -->
+      <div class="col-md-3 bg-light h-100">
+        <div class="h-25">
+          <div id="map" style="height: 100%"></div>
+        </div>
+        <div class="h-50">
+          <div class="card h-100 mb-3">
+            <div class="card-header bg-secondary text-white">
+              <h3 class="h5 mb-0">Weather Today</h3>
+            </div>
+            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+              <div class="display-4 font-weight-bold text-primary">22°C</div>
+              <p class="mt-3">Partly Cloudy</p>
+            </div>
+          </div>
+        </div>
+        <div class="h-25">
+          <div class="card h-100 mb-3">
+            <div class="card-header bg-secondary text-white">
+              <h3 class="h5 mb-0">Weather This Week</h3>
+            </div>
+            <div class="card-body p-0">
+              <table class="table table-striped table-borderless m-0">
+                <tbody>
+                  <tr>
+                    <td>Monday</td>
+                    <td>19°C</td>
+                    <td>Mostly Cloudy</td>
+                  </tr>
+                  <tr>
+                    <td>Tuesday</td>
+                    <td>21°C</td>
+                    <td>Partly Cloudy</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday</td>
+                    <td>23°C</td>
+                    <td>Sunny</td>
+                  </tr>
+                  <tr>
+                    <td>Thursday</td>
+                    <td>20°C</td>
+                    <td>Partly Cloudy</td>
+                  </tr>
+                  <tr>
+                    <td>Friday</td>
+                    <td>18°C</td>
+                    <td>Mostly Cloudy</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-
-    <div class="row">
-      <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="card mb-3">
-          <div class="card-header bg-secondary text-white">
-            <h3 class="mb-0">Weather Info</h3>
-          </div>
-          <div class="card-body">
-            <div id="map" style="height: 200px"></div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-lg-9 col-md-8 col-sm-6 col-xs-12">
+      <!-- Widgets -->
+      <div class="col-md-9">
         <div class="row">
           <div class="col-md-6">
             <div class="card mb-3">
               <div class="card-body d-flex justify-content-between align-items-center">
                 <div class="d-flex flex-column">
-                  <h3 class="mb-0">Leases</h3>
-                  <div class="progress">
+                  <h3 class="h5 mb-0">Leases</h3>
+                  <div class="progress my-2">
                     <div
                       class="progress-bar bg-danger"
                       role="progressbar"
@@ -46,8 +83,8 @@
             <div class="card mb-3">
               <div class="card-body d-flex justify-content-between align-items-center">
                 <div class="d-flex flex-column">
-                  <h3 class="mb-0">Offers</h3>
-                  <div class="progress">
+                  <h3 class="h5 mb-0">Offers</h3>
+                  <div class="progress my-2">
                     <div
                       class="progress-bar bg-primary"
                       role="progressbar"
@@ -65,7 +102,7 @@
           <div class="col-md-6">
             <div class="card mb-3">
               <div class="card-body">
-                <h3 class="mb-0">Sales</h3>
+                <h3 class="h5 mb-0">Sales</h3>
                 <div class="d-flex align-items-center justify-content-center h-100">
                   <canvas id="rainfall-chart" style="height: 150px"></canvas>
                 </div>
@@ -75,7 +112,7 @@
           <div class="col-md-6">
             <div class="card mb-3">
               <div class="card-body">
-                <h3 class="mb-0">Another Container</h3>
+                <h3 class="h5 mb-0">Another Container</h3>
                 <div class="d-flex align-items-center justify-content-center h-100">
                   <canvas id="crop-yield-chart" style="height: 150px"></canvas>
                 </div>
@@ -87,11 +124,6 @@
     </div>
   </div>
 </template>
-
-<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js"></script>
-<script src="https://api.mapbox.com/mapbox-gl-js/v2.6.0/mapbox-gl.js"></script> -->
-
 <script>
 import mapboxgl from 'mapbox-gl'
 mapboxgl.accessToken =
