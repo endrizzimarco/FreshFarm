e91d1029a2c98b2b4814785d47b4107485601b49
az00379 <az00379@surrey.ac.uk>
2023-04-08 19:07:11 +0100

    feat: final commit before submitting a problem on github

--- backend/python/Dashboard/__init__.py
+++ backend/python/Dashboard/__init__.py
@@ -4,30 +4,29 @@ from azure.cosmos import CosmosClient
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Initialize the Cosmos DB client
-    endpoint = 'https://myaccount.documents.azure.com:443/'
-    key = 'mykey'
+    endpoint = 'https://freshfarmdb.documents.azure.com:443/'
+    key = '4g8dCbTnj1b3PPEVdmbEDmrCahKDYW1xvdlBnZQhL5Szl1tkRCUEUzH4IDgLP2dyIh4VyUQXtM1fACDbEEKA5A=='
     client = CosmosClient(endpoint, key)
 
     # Get a reference to the sales container
-    database_name = 'mydatabase'
-    container_name = 'mycontainer'
+    database_name = 'FreshFarmDB'
+    container_name = 'Sales'
     database = client.get_database_client(database_name)
     container = database.get_container_client(container_name)
 
     # Execute a query to retrieve sales data
-    query = 'SELECT * FROM Sales'
+    query = 'SELECT * FROM c'
     items = container.query_items(query, enable_cross_partition_query=True)
 
     # Format the sales data as needed
     sales_data_formatted = []
     for item in items:
         sale = {
-            'id': item['id'],
-            'date': item['date'],
-            'product': item['product'],
-            'quantity': item['quantity'],
+            'farmerId': item['farmerId'],
+            'customerName': item['customerName'],
+            'type': item['type'],
+            'collectionTime': item['collectionTime'],
             'price': item['price'],
-            'total': item['total']
         }
         sales_data_formatted.append(sale)
 
--- frontend/src/pages/FarmersDashboard.vue
+++ frontend/src/pages/FarmersDashboard.vue
@@ -1,147 +1,68 @@
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-3 bg-light h-100">
-        <q-card class="map-card">
-          <q-card-section>
-            <div id="map" style="height: 200px"></div>
-          </q-card-section>
-        </q-card>
-        <q-card class="h-50">
-          <q-card-section class="mt-3 my-styles">☀️ Weather Forecast</q-card-section>
-          <q-card-section>
-            <div class="d-flex justify-content-center align-items-center">
-              <div class="weather-cards row">
-                <div v-for="day in weatherForecast" :key="day.date" class="weather-card col">
-                  <div class="weather-card__date">{{ day.day }}</div>
-                  <div class="weather-card__temp">{{ day.temp }}</div>
-                </div>
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
-
-        <br />
-        <q-card class="h-25">
-          <q-card-section class="mt-3 my-styles">🛰️ Weather This Week</q-card-section>
-          <q-card-section>
-            <q-table
-              :data="weatherForecast"
-              :columns="weatherColumns"
-              row-key="date"
-              :hide-bottom="true"
-              :grid="$q.screen.gt.xs"
-            ></q-table>
-          </q-card-section>
-        </q-card>
-      </div>
-      <!-- Widgets -->
-      <div class="col-md-9">
-        <h2 class="mb-4" style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 48px">
-          &nbsp;&nbsp;📊 Farmers Dashboard
-        </h2>
-
-        <div class="row">
-          <div class="col-md-6">
-            <q-card class="mb-3 border">
-              <q-card-section>
-                <div class="text-h4">&nbsp;Top 10 Orders</div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-md-6">
-            <q-card class="mb-3 border">
-              <q-card-section>
-                <div class="text-h4">&nbsp;Top 10 Selling Items</div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-md-6">
-            <q-card class="mb-3 border">
-              <q-card-section>
-                <div class="text-h4">&nbsp;Total Revenue</div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-md-6">
-            <q-card class="mb-3 border">
-              <q-card-section>
-                <div class="text-h4">&nbsp;Average Time of Sales</div>
-              </q-card-section>
-            </q-card>
-          </div>
-          <div class="col-md-6">
-            <q-card class="mb-3 border">
-              <q-card-section>
-                <div class="text-h4">&nbsp;Top 4 Items Sold In The Area</div>
-              </q-card-section>
-            </q-card>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <q-table
+    :data="salesData"
+    :columns="columns"
+    v-model:pagination="pagination"
+    :rows-per-page-options="[10, 25, 50, 100]"
+  />
 </template>
 
 <script>
-import mapboxgl from 'mapbox-gl'
-
 export default {
-  name: 'FarmersDashboard',
+  name: 'SalesTable',
   data() {
     return {
-      weatherForecast: [],
-      weatherColumns: [
+      salesData: [],
+      pagination: {
+        rowsPerPage: 10
+      },
+      columns: [
         {
-          name: 'day',
-          required: true,
-          label: 'Day',
+          name: 'farmerId',
+          label: 'Farmer ID',
           align: 'left',
-          field: row => row.day
+          field: 'farmerId'
         },
         {
-          name: 'temp',
-          required: true,
-          label: 'Temperature',
+          name: 'customerName',
+          label: 'Customer Name',
           align: 'left',
-          field: row => row.temp
+          field: 'customerName'
         },
         {
-          name: 'description',
-          required: true,
-          label: 'Description',
+          name: 'type',
+          label: 'Type',
           align: 'left',
-          field: row => row.description
+          field: 'type'
+        },
+        {
+          name: 'collectionTime',
+          label: 'Collection Time',
+          align: 'left',
+          field: 'collectionTime'
+        },
+        {
+          name: 'price',
+          label: 'Price',
+          align: 'left',
+          field: 'price'
         }
       ]
     }
   },
   mounted() {
-    const apiKey = '9a4d1285248745b1b38134758230504'
-    const city = 'New York'
-    const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.weatherForecast = data.forecast.forecastday.map(day => {
-          return {
-            day: day.date,
-            temp: `${day.day.avgtemp_c}°C`,
-            description: day.day.condition.text
-          }
-        })
-      })
-      .catch(error => console.error(error))
-
-    mapboxgl.accessToken =
-      'pk.eyJ1IjoiYWJkdWxsYWh6YWhpZDEwIiwiYSI6ImNsZjJ5M3UybzBuYWczc256MXdpMGZkMGsifQ.sUQa5jZuO5tn1Nnbjw6CLw'
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-73.985664, 40.748817],
-      zoom: 12
-    })
+    this.fetchSalesData()
+  },
+  methods: {
+    async fetchSalesData() {
+      try {
+        const response = await fetch('/api/get-sales-data')
+        const data = await response.json()
+        this.salesData = data
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 }
 </script>
