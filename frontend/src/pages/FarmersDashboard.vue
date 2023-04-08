0231d4297158194efdefec0a3e804fec6460b4d6
az00379 <az00379@surrey.ac.uk>
2023-04-08 16:42:28 +0100

    style: made changes to the code, aded more extensive quasar principles

--- frontend/src/pages/FarmersDashboard.vue
+++ frontend/src/pages/FarmersDashboard.vue
@@ -2,63 +2,37 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col-md-3 bg-light h-100">
-        <div class="card-body mt-0">
-          <div id="map" style="height: 200px"></div>
-        </div>
-        <div class="h-50">
-          <div class="card h-100">
-            <br />
-            <p class="mt-3 my-styles">&nbsp;☀️ Weather Forecast</p>
-            <div class="card-body d-flex justify-content-center align-items-center">
-              <div style="display: flex; justify-content: center; align-items: center">
-                <div style="display: flex; justify-content: center; align-items: center">
-                  <div v-for="day in weatherForecast" :key="day.date" class="weather-card">
-                    <div class="weather-card__date">{{ day.day }}</div>
-                    <div class="weather-card__temp">{{ day.temp }}</div>
-                  </div>
+        <q-card class="map-card">
+          <q-card-section>
+            <div id="map" style="height: 200px"></div>
+          </q-card-section>
+        </q-card>
+        <q-card class="h-50">
+          <q-card-section class="mt-3 my-styles">☀️ Weather Forecast</q-card-section>
+          <q-card-section>
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="weather-cards">
+                <div v-for="day in weatherForecast" :key="day.date" class="weather-card">
+                  <div class="weather-card__date">{{ day.day }}</div>
+                  <div class="weather-card__temp">{{ day.temp }}</div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          </q-card-section>
+        </q-card>
         <br />
-        <br />
-        <div class="h-25">
-          <div class="card h-100 mb-3">
-            <p class="mt-3 my-styles">&nbsp;🛰️ Weather This Week</p>
-            <div class="card-body p-0">
-              <table class="table table-striped table-borderless m-0">
-                <tbody>
-                  <tr>
-                    <td>Monday</td>
-                    <td>19°C</td>
-                    <td>Mostly Cloudy</td>
-                  </tr>
-                  <tr>
-                    <td>Tuesday</td>
-                    <td>21°C</td>
-                    <td>Partly Cloudy</td>
-                  </tr>
-                  <tr>
-                    <td>Wednesday</td>
-                    <td>23°C</td>
-                    <td>Sunny</td>
-                  </tr>
-                  <tr>
-                    <td>Thursday</td>
-                    <td>20°C</td>
-                    <td>Partly Cloudy</td>
-                  </tr>
-                  <tr>
-                    <td>Friday</td>
-                    <td>18°C</td>
-                    <td>Mostly Cloudy</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <q-card class="h-25">
+          <q-card-section class="mt-3 my-styles">🛰️ Weather This Week</q-card-section>
+          <q-card-section>
+            <q-table
+              :data="weatherForecast"
+              :columns="weatherColumns"
+              row-key="date"
+              :hide-bottom="true"
+              :grid="$q.screen.gt.xs"
+            ></q-table>
+          </q-card-section>
+        </q-card>
       </div>
       <!-- Widgets -->
       <div class="col-md-9">
@@ -68,80 +42,46 @@
 
         <div class="row">
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body d-flex justify-content-between align-items-center">
-                <div class="d-flex flex-column">
-                  <h3
-                    class="h5 mb-0"
-                    style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                  >
-                    &nbsp;Leases
-                  </h3>
-                </div>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Top 10 Orders</div>
+              </q-card-section>
+            </q-card>
           </div>
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body d-flex justify-content-between align-items-center">
-                <div class="d-flex flex-column">
-                  <h3
-                    class="h5 mb-0"
-                    style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                  >
-                    &nbsp;Offers
-                  </h3>
-                </div>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Top 10 Selling Items</div>
+              </q-card-section>
+            </q-card>
           </div>
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body">
-                <h3
-                  class="h5 mb-0"
-                  style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                >
-                  &nbsp;Sales
-                </h3>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Total Revenue</div>
+              </q-card-section>
+            </q-card>
           </div>
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body">
-                <h3
-                  class="h5 mb-0"
-                  style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                >
-                  &nbsp;Another Container
-                </h3>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Top 10 Competitors</div>
+              </q-card-section>
+            </q-card>
           </div>
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body">
-                <h3
-                  class="h5 mb-0"
-                  style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                >
-                  &nbsp;Sales
-                </h3>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Average Time of Sales</div>
+              </q-card-section>
+            </q-card>
           </div>
           <div class="col-md-6">
-            <div class="card mb-3 border">
-              <div class="card-body">
-                <h3
-                  class="h5 mb-0"
-                  style="font-weight: bold; font-family: 'Helvetica Neue', Arial, sans-serif; font-size: 36px"
-                >
-                  &nbsp;Another Container
-                </h3>
-              </div>
-            </div>
+            <q-card class="mb-3 border">
+              <q-card-section>
+                <div class="text-h4">&nbsp;Top 4 Competitors</div>
+              </q-card-section>
+            </q-card>
           </div>
         </div>
       </div>
@@ -156,7 +96,30 @@ export default {
   name: 'FarmersDashboard',
   data() {
     return {
-      weatherForecast: []
+      weatherForecast: [],
+      weatherColumns: [
+        {
+          name: 'day',
+          required: true,
+          label: 'Day',
+          align: 'left',
+          field: row => row.day
+        },
+        {
+          name: 'temp',
+          required: true,
+          label: 'Temperature',
+          align: 'left',
+          field: row => row.temp
+        },
+        {
+          name: 'description',
+          required: true,
+          label: 'Description',
+          align: 'left',
+          field: row => row.description
+        }
+      ]
     }
   },
   mounted() {
