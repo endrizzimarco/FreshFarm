9d0e514132b95c055fd5b87543199e53afd86354
az00379 <az00379@surrey.ac.uk>
2023-04-11 13:23:28 +0100

    refactor: testing total revenue microservice

---
+++ backend/python/total-revenue/__init__.py
@@ -0,0 +1,16 @@
+import azure.functions as func
+import logging
+import json
+
+
+def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
+    sales_json = []
+    for offer in doc:
+        sales_json.append({
+            "customerName": offer['total'],
+        })
+
+    return func.HttpResponse(
+            json.dumps(sales_json),
+            status_code=200,
+            mimetype="application/json")
