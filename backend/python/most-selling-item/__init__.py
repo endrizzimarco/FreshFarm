b0840817db68467b5c5584ad0933099bb6bfd979
az00379 <az00379@surrey.ac.uk>
2023-04-12 15:46:26 +0100

    refactor: made amendments to most selling microservice

---
+++ backend/python/most-selling-item/__init__.py
@@ -0,0 +1,17 @@
+import azure.functions as func
+import logging
+import json
+
+
+def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
+    sales_json = []
+    for offer in doc:
+        sales_json.append({
+            "type": offer['type'],
+            "price": offer['total_price'],
+        })
+
+    return func.HttpResponse(
+            json.dumps(sales_json),
+            status_code=200,
+            mimetype="application/json")
