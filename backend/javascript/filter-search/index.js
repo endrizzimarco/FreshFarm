163cb4a40c9d194cfd37f3f6a2a46f473a28bbda
mimam <mnimam321@gmail.com>
2023-04-13 14:45:05 +0400

    Refactor: Filter-Search code added

--- backend/javascript/filter-search/index.js
+++ backend/javascript/filter-search/index.js
@@ -1,13 +1,21 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-}
+class Function {
+    static async filterSearch(Offers, filter) {
+      const querySpec = {
+        query: `SELECT c.farmerId, c.Lat, c.Lng, c.Price, c.Type, c.Items FROM c WHERE ${filter}`
+      };
+      const { resources: results } = await client
+        .database("FreshFarmDB")
+        .container(Offers)
+        .items.query(querySpec)
+        .fetchAll();
+  
+      return results;
+    }
+  }
+  
+  // Example usage:
+  const collection = "Offers";
+  const filter = "c.farmerId = 'Paul' AND c.Price < 5";
+  const filteredResults = await Function.filterSearch(Offers, filter);
+  console.log(filteredResults);
+  
