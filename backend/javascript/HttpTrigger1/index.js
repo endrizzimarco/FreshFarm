967f37a2f5f4ab1d58cd0198f38f696de7b19ffa
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-04-13 23:19:19 +0100

    feat: js function filters offers on max price

--- backend/javascript/HttpTrigger1/index.js
+++ backend/javascript/HttpTrigger1/index.js
@@ -1,22 +1,21 @@
 module.exports = async function (context, req) {
-	context.log("JavaScript HTTP trigger function processed a request.");
+  context.log('JavaScript HTTP trigger function processed a request.')
 
-	const name = req.query.name || (req.body && req.body.name);
-	const responseMessage = name
-		? "HELlo, " + name + ". This HTTP triggered function executed successfully."
-		: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+  const name = req.query.name || (req.body && req.body.name)
+  const responseMessage = name
+    ? 'HELlo, ' + name + '. This HTTP triggered function executed successfully.'
+    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
+  console.log('asd', context.bindings)
+  // if (name) {
+  //   context.bindings.outputDocument = JSON.stringify({
+  //     // create a random ID
+  //     farmerId: new Date().toISOString() + Math.random().toString().substring(2, 10),
+  //     name: name
+  //   })
+  // }
 
-	if (name) {
-		context.bindings.outputDocument = JSON.stringify({
-			// create a random ID
-			farmerId:
-				new Date().toISOString() + Math.random().toString().substring(2, 10),
-			name: name,
-		});
-	}
-
-	context.res = {
-		// status: 200, /* Defaults to 200 */
-		body: responseMessage,
-	};
-};
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: context.bindings.OffersQuery
+  }
+}
