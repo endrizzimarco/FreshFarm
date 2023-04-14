e429ecb8f597a4f6a9af02c0e680c3ac387dabe3
az00379 <az00379@surrey.ac.uk>
2023-04-14 02:19:01 +0100

    refactor: msi

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -4,13 +4,17 @@ import json
 
 
 def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    logging.info(json.dumps(doc))
+
     sales_json = []
 
-    for item in doc:
-        sales_json.append({
-            "type": item["type"],
-            "total_price": item["total_price"]
-        })
+    for doc_item in doc:
+        for item in doc_item:
+            sales_json.append({
+                "type": item["type"],
+                "total_price": item["total_price"]
+            })
 
     return func.HttpResponse(
         json.dumps(sales_json),
