7431a67f79104b8b0cf03672a38172ea5aedd2d8
az00379 <az00379@surrey.ac.uk>
2023-04-14 16:50:12 +0100

    refactor: msi

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -1,10 +1,18 @@
 import azure.functions as func
 import logging
+import json
 
 
 def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
-    query = doc.get('sqlQuery', None)
-    if not query:
-        return func.HttpResponse("SQL query not found in input binding.", status_code=400)
-    
-    return func.HttpResponse(query, status_code=200, mimetype="text/plain")
+    sales_json = []
+    for offer in doc:
+        sales_json.append({
+            "customerName": offer['customerName'],
+            "type": offer['type'],
+            "price": offer['price'],
+        })
+
+    return func.HttpResponse(
+            json.dumps(sales_json),
+            status_code=200,
+            mimetype="application/json")
