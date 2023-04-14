17f84821c60fe1233724d9d1945073be2457f3cd
az00379 <az00379@surrey.ac.uk>
2023-04-14 17:01:26 +0100

    REFACTOR: MSI

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -1,16 +1,14 @@
 import azure.functions as func
 import logging
-import json
 
 
 def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
     output = ""
     for item in doc:
-        output += json.dumps(item) + "\n"
+        output += str(item) + "\n"
     
     return func.HttpResponse(
         output,
         status_code=200,
         mimetype="text/plain"
     )
-# 
