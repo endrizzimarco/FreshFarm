ea7ef9ff82946a60a52826c8eadec51fd014a5d6
az00379 <az00379@surrey.ac.uk>
2023-04-14 16:56:58 +0100

    refactor: msi

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -1,11 +1,12 @@
 import azure.functions as func
 import logging
+import json
 
 
 def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
     output = ""
     for item in doc:
-        output += str(item) + "\n"
+        output += json.dumps(item) + "\n"
     
     return func.HttpResponse(
         output,
