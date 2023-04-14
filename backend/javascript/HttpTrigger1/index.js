29cbfb5b7691d392f0f3464e80c2267797d55962
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-04-14 13:32:07 +0100

    feat: filter function filtering on type, price and radius

--- backend/javascript/HttpTrigger1/index.js
+++ backend/javascript/HttpTrigger1/index.js
@@ -1,21 +1,96 @@
+const { CosmosClient } = require('@azure/cosmos')
+const dbClient = new CosmosClient(process.env.CosmosDBConnectionString)
+const offersContainer = dbClient.database('FreshFarmDB').container('Offers')
+
+function toRadians(degrees) {
+  return (degrees * Math.PI) / 180.0
+}
+
+function toDegrees(radians) {
+  return (radians * 180.0) / Math.PI
+}
+
+function boundingBox(lat, lng, radius) {
+  const R = 3963.0 // Earth's radius in miles
+  const latRad = toRadians(lat)
+  const lngRad = toRadians(lng)
+
+  const angularDistance = radius / R
+
+  const latDelta = toDegrees(angularDistance)
+  const lngDelta = toDegrees(angularDistance / Math.cos(latRad))
+
+  const maxLat = Number(lat) + latDelta
+  const minLat = lat - latDelta
+  const maxLng = Number(lng) + lngDelta
+  const minLng = lng - lngDelta
+
+  return {
+    maxLat: maxLat,
+    minLat: minLat,
+    maxLng: maxLng,
+    minLng: minLng
+  }
+}
+
 module.exports = async function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
 
-  const name = req.query.name || (req.body && req.body.name)
-  const responseMessage = name
-    ? 'HELlo, ' + name + '. This HTTP triggered function executed successfully.'
-    : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.'
-  console.log('asd', context.bindings)
-  // if (name) {
-  //   context.bindings.outputDocument = JSON.stringify({
-  //     // create a random ID
-  //     farmerId: new Date().toISOString() + Math.random().toString().substring(2, 10),
-  //     name: name
-  //   })
-  // }
+  const maxPrice = req.query.maxPrice || (req.body && req.body.maxPrice)
+  const maxRadius = req.query.maxRadius || (req.body && req.body.maxRadius)
+  const type = req.query.type || (req.body && req.body.type)
+  const lat = req.query.lat || (req.body && req.body.lat)
+  const lng = req.query.lng || (req.body && req.body.lng)
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: context.bindings.OffersQuery
+  function typeValid() {
+    return type.match(/^(Dairy|Eggs|Meat|Grain|Vegetables|Fruit|Other)$/)
+  }
+
+  let filterQuery = `SELECT * FROM c `
+  if (!maxPrice && !maxRadius && !type) {
+    context.res = {
+      status: 400,
+      body: 'No filter parameters provided in query string or body.'
+    }
+  } else if (type && !typeValid()) {
+    context.res = {
+      status: 400,
+      body: 'Invalid type provided.'
+    }
+  } else if (maxRadius && (!lat || !lng)) {
+    context.res = {
+      status: 400,
+      body: 'Radius provided but no lat/lng provided.'
+    }
+  } else {
+    if (maxPrice && !maxRadius && !type) {
+      filterQuery += ` WHERE c.Price <= ${maxPrice} `
+    } else if (!maxPrice && maxRadius && lat && lng && !type) {
+      //TODO: implement radius
+      const bb = boundingBox(lat, lng, maxRadius)
+      filterQuery += ` WHERE c.Lat <= ${bb.maxLat} AND c.Lat >= ${bb.minLat} AND c.Lng <= ${bb.maxLng} AND c.Lng >= ${bb.minLng}`
+    } else if (!maxPrice && !maxRadius && type) {
+      filterQuery += ` WHERE c.Type = '${type}' `
+    } else if (maxPrice && !maxRadius && type) {
+      filterQuery += ` WHERE c.Price <= ${maxPrice} AND c.Type = '${type}' `
+    } else if (maxPrice && maxRadius && lat && lng && !type) {
+      //TODO: implement radius
+      const bb = boundingBox(lat, lng, maxRadius)
+      filterQuery += ` WHERE c.Price <= ${maxPrice} AND c.Lat <= ${bb.maxLat} AND c.Lat >= ${bb.minLat} AND c.Lng <= ${bb.maxLng} AND c.Lng >= ${bb.minLng}`
+    } else if (!maxPrice && maxRadius && lat && lng && type) {
+      //TODO: implement radius
+      const bb = boundingBox(lat, lng, maxRadius)
+      filterQuery += ` WHERE c.Type = '${type}' AND c.Lat <= ${bb.maxLat} AND c.Lat >= ${bb.minLat} AND c.Lng <= ${bb.maxLng} AND c.Lng >= ${bb.minLng}`
+    } else if (maxPrice && maxRadius && lat && lng && type) {
+      //TODO: implement radius
+      const bb = boundingBox(lat, lng, maxRadius)
+      filterQuery += ` WHERE c.Price <= ${maxPrice} AND c.Type = '${type}' AND c.Lat <= ${bb.maxLat} AND c.Lat >= ${bb.minLat} AND c.Lng <= ${bb.maxLng} AND c.Lng >= ${bb.minLng}`
+    }
+    console.log(filterQuery)
+    const dbstuff = await offersContainer.items.query(filterQuery).fetchAll()
+    context.res = {
+      // status: 200, /* Defaults to 200 */
+      body: dbstuff.resources
+    }
   }
 }
