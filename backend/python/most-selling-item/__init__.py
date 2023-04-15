6e9127240faa887d268af9426ab77499ecff4126
az00379 <az00379@surrey.ac.uk>
2023-04-15 14:34:14 +0100

    refactor: ms

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -1,18 +1,26 @@
 import azure.functions as func
-import logging
 import json
 
+def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
+    # Initialize a dictionary to store the total sales for each type
+    sales_by_type = {}
 
-def main(req: func.HttpRequest, doc:func.DocumentList) -> func.HttpResponse:
-    sales_json = []
-    for offer in doc:
-        sales_json.append({
-            # "customerName": offer['customerName'],
-            "type": offer['type'],
-            "price": offer['price'],
-        })
+    # Iterate over the document list and sum the prices for each type
+    for item in doc:
+        item_type = item.get('type')
+        item_price = item.get('price', 0)
 
+        if item_type in sales_by_type:
+            sales_by_type[item_type] += item_price
+        else:
+            sales_by_type[item_type] = item_price
+
+    # Convert the dictionary to a list of dictionaries with 'type' and 'total_price' keys
+    sales_json = [{'type': k, 'total_price': v} for k, v in sales_by_type.items()]
+
+    # Return the results as a JSON response
     return func.HttpResponse(
-            json.dumps(sales_json),
-            status_code=200,
-            mimetype="application/json")
+        json.dumps(sales_json),
+        status_code=200,
+        mimetype="application/json"
+    )
