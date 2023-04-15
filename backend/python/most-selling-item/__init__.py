959179d7dfc0023237250bbc80da7903e4c2878f
az00379 <az00379@surrey.ac.uk>
2023-04-15 14:13:31 +0100

    refactor: msi

--- backend/python/most-selling-item/__init__.py
+++ backend/python/most-selling-item/__init__.py
@@ -1,18 +1,18 @@
 import azure.functions as func
-from azure.cosmos import CosmosClient
+import logging
+import json
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    # Define your Cosmos DB endpoint, key, and database and container names
-    endpoint = 'https://freshfarmdb.documents.azure.com:443/'
-    key = '4g8dCbTnj1b3PPEVdmbEDmrCahKDYW1xvdlBnZQhL5Szl1tkRCUEUzH4IDgLP2dyIh4VyUQXtM1fACDbEEKA5A=='
-    database_name = 'FreshFarmDB'
-    container_name = 'Sales'
 
-    # Create a Cosmos DB client and retrieve the data using a SQL query
-    client = CosmosClient(endpoint, key)
-    container = client.get_database_client(database_name).get_container_client(container_name)
-    query = "SELECT c.type, SUM(c.price) AS total_price FROM c GROUP BY c.type"
-    items = list(container.query_items(query=query, enable_cross_partition_query=True))
-
-    # Return the results as a JSON response
-    return func.HttpResponse(body=str(items), mimetype="application/json")
+def main(req: func.HttpRequest, doc: func.DocumentList) -> func.HttpResponse:
+    output = []
+    for item in doc:
+        output.append({
+            "type": item['type'],
+            # "total_price": item['total_price']
+        })
+    
+    return func.HttpResponse(
+        json.dumps(output),
+        status_code=200,
+        mimetype="application/json"
+    )
