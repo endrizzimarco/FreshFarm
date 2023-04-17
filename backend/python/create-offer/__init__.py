0aadbfd7f39c03cabda86d41d8b6848d7009b246
Marco Endrizzi <marco.endrizzi.bz@gmail.com>
2023-04-17 12:04:04 +0200

    refactor: add uuid
    Co-authored-by: Georgi Ivanov <georgi-ivanov19@users.noreply.github.com>

--- backend/python/create-offer/__init__.py
+++ backend/python/create-offer/__init__.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 import logging
+import uuid 
 import json
 
 def main(req: func.HttpRequest, outputDocument: func.Out[func.Document], actions: func.Out[str]) -> func.HttpResponse:
@@ -8,16 +9,18 @@ def main(req: func.HttpRequest, outputDocument: func.Out[func.Document], actions
     allowed_keys = ["id", "farmerId", "lat", "lng", "price", "type", "items", "description", "pictureUrl", "title"]
     req_body = req.get_json()
     new_offer = {k: req_body[k] for k in allowed_keys if k in req_body}
+    new_offer["id"] = uuid.uuid4()
     logging.info(f"New offer: {new_offer}")
 
     if req:
-        offer = outputDocument.set(func.Document.from_dict(new_offer))
+        # write new_offer to cosmosdb and return generated item 
+        outputDocument.set(func.Document.from_dict(new_offer))
         actions.set(json.dumps({
         'actionName': 'sendToAll',
-        'data': json.dumps(offer),
+        'data': json.dumps(new_offer),
         'dataType': 'text'
     }))
-        return func.HttpResponse(f"Offer {offer} successfully created!")
+        return func.HttpResponse(f"Offer {new_offer} successfully created!")
     else:
         return func.HttpResponse(
                     "Please pass a JSON with the new offer in the request body",
