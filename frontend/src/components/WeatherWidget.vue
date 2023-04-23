a1afb9e644096967a115547deca3647b000c8b67
Georgi Ivanov <66921360+georgi-ivanov66@users.noreply.github.com>
2023-04-23 10:41:05 +0100

    chore: formatting

--- frontend/src/components/WeatherWidget.vue
+++ frontend/src/components/WeatherWidget.vue
@@ -3,12 +3,12 @@ q-card.p-2
   q-card-section
     p.text-lg.font-semibold.text-gray-200 {{fullWeatherForecast?.location.name}} &nbsp;
       q-icon(name="near_me")
-    .float-left 
+    .float-left
       p.text-sm.pt-1 Currently
       p.text-xl {{fullWeatherForecast?.current.temp_c}}°
     .float-right
       p.text-sm {{fullWeatherForecast?.current.condition.text}}
-      q-icon(:name="'img:'+currentWeatherIcon" style="font-size: 2em") 
+      q-icon(:name="'img:'+currentWeatherIcon" style="font-size: 2em")
   br
   q-card-section
     .row.pt-2
@@ -20,7 +20,7 @@ q-card.p-2
     p.text-sm.pt-5 Next 5 days
     .row
       .col.text-center(v-for='d in nextFiveDaysFcast' :key='d.date')
-        p.text-xs.pt-2 {{new Date(d.date).toLocaleString('default', {day: 'numeric', month: 'short'})}}   
+        p.text-xs.pt-2 {{new Date(d.date).toLocaleString('default', {day: 'numeric', month: 'short'})}}
         q-icon.pt-1(:name="'img:'+d.day.condition.icon" style="font-size: 2em")
         p.text.ml-1 {{d.day.avgtemp_c}}°
 </template>
